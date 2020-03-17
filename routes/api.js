1b928b8811af3dd9e5355373971d8062f3758ab1
Maciej Jamer <mjamer@concisesoftware.com>
2020-03-17 14:51:24 +0100

    after 10 video

--- routes/api.js
+++ routes/api.js
@@ -30,4 +30,24 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.get('/login', (req, res) => {
+    const userData = req.body;
+
+    User.findOne({
+        email: userData.email
+    }, (error, user) => {
+        if ( error ) {
+            console.log(error);
+        } else {
+            if ( !user ) {
+                res.status(401).send('Invalid email');
+            } else if ( user.password !== userData.password ) { 
+                res.status(401).send('Invalid password');
+            } else {
+                res.status(200).send(user);
+            }
+        }
+    });
+});
+
 module.exports = router;
