dbe8a0d9b4073ebbae185b7538daa209dfc5c62e
Maciej Jamer <mjamer@concisesoftware.com>
2020-03-17 13:48:44 +0100

    after 7 video

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 const mongooes = require('mongoose');
 const db = 'mongodb://admin:admin1@ds229438.mlab.com:29438/eventsdb';
 
@@ -14,6 +16,18 @@ mongooes.connect(db, err => {
 
 router.get('/', (req, res) => {
     res.send('From API route');
+});
+
+router.post('/register', (req, res) => {
+    const userData = req.body;
+    const user = new User(userData);
+    user.save((error, registeredUser) => {
+        if ( error ) {
+            console.log(error);
+        } else {
+            res.status(200).send(registeredUser);
+        }
+    });
 });
 
 module.exports = router;
