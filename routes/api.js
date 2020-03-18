c4d3ffca6fd22a760ae0412dad9940f39622ae7b
Maciej Jamer <mjamer@concisesoftware.com>
2020-03-18 08:21:50 +0100

    after 22 video

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const User = require('../models/user');
@@ -25,7 +26,11 @@ router.post('/register', (req, res) => {
         if ( error ) {
             console.log(error);
         } else {
-            res.status(200).send(registeredUser);
+          const payload = {
+            subject: registeredUser._id
+          }
+          const token = jwt.sign(payload, 'secretKey')
+          res.status(200).send(token);
         }
     });
 });
@@ -44,7 +49,11 @@ router.post('/login', (req, res) => {
             } else if ( user.password !== userData.password ) { 
                 res.status(401).send('Invalid password');
             } else {
-                res.status(200).send(user);
+              const payload = {
+                subject: user.id
+              }
+              const token = jwt.sign(payload, 'secretKey');
+              res.status(200).send(token);
             }
         }
     });
